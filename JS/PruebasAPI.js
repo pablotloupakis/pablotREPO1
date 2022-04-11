f21051ef5f6bdeb114a2ac81a572c6cf26cbcf01
pablotloupakis <pablotloupakis@hotmail.com>
2022-04-11 12:02:35 -0400

    VAMOS

--- JS/PruebasAPI.js
+++ JS/PruebasAPI.js
@@ -1,2 +1,14 @@
 "use strict";
 console.log ("Hello API"); 
+
+async function funcPablot(){
+	url = 'https://catfact.ninja/fact';
+	let promisePABLOT = await fetch(url); 
+	if (promisePABLOT.status === 200){
+		let strCATFact = await (promisePABLOT.text()); 
+		console.log (strCATFact); 
+	}else{
+		console.log ("ERRORES!"); 
+	}
+}
+funcPablot(); 
